2eec42fccc538a2b9af52e2e136bc0de50b10ecf
Malificiece <Garrett_Benoit@baylor.edu>
2013-11-06 18:04:01 -0600

    Working on a RigidBody class

--- workspace/MazeGameServer/src/engine/physics/RigidBody.java
+++ workspace/MazeGameServer/src/engine/physics/RigidBody.java
@@ -1,5 +1,20 @@
 package engine.physics;
 
+import engine.Position;
+
 public class RigidBody {
+    private Position<Float, Float> max;
+    private Position<Float, Float> location;
+    private Position<Float, Float> min;
+    protected float radius;
+    protected float width;
+    protected float height;
+    protected float velY;
+    protected float velX;
+    protected float deltaX;
+    protected float deltaY;
     
+    public RigidBody() {
+        
+    }
 }
